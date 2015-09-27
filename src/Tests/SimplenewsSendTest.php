7d1048312bcb9e9cae491e896b248078715d7e1a
Sascha Grossenbacher <saschagros@gmail.com>
2015-09-27 15:58:48 +0200

    Temporarily run strip_tags() on expected mail subject

--- src/Tests/SimplenewsSendTest.php
+++ src/Tests/SimplenewsSendTest.php
@@ -335,7 +335,9 @@ class SimplenewsSendTest extends SimplenewsTestBase {
     $mails = $this->drupalGetMails();
     $this->assertEqual(5, count($mails), t('All mails were sent.'));
     foreach ($mails as $mail) {
-      $this->assertEqual($mail['subject'], '[Default newsletter] ' . $edit['title[0][value]'], t('Mail has correct subject'));
+      // @todo Temporarily strip tags from mail subjects until
+      //   https://www.drupal.org/node/2575791 is fixed.
+      $this->assertEqual($mail['subject'], '[Default newsletter] ' . strip_tags($edit['title[0][value]']), t('Mail has correct subject'));
       $this->assertTrue(isset($this->subscribers[$mail['to']]), t('Found valid recipient'));
       unset($this->subscribers[$mail['to']]);
     }
