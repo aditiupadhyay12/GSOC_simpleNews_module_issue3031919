551dff2e6cf6da3fc01e24c22e250907db18c2c8
sardara <sardara@2353864.no-reply.drupal.org>
2018-03-17 14:16:32 +0100

    Issue #2914814 by sardara: Avoid double sync of fields from subscription to user when user is being saved

--- src/Entity/Subscriber.php
+++ src/Entity/Subscriber.php
@@ -178,6 +178,13 @@ class Subscriber extends ContentEntityBase implements SubscriberInterface {
   /**
    * {@inheritdoc}
    */
+  public function setSyncing($sync = TRUE) {
+    static::$syncing = $sync;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function isSubscribed($newsletter_id) {
     foreach ($this->subscriptions as $item) {
       if ($item->target_id == $newsletter_id) {
@@ -274,7 +281,7 @@ class Subscriber extends ContentEntityBase implements SubscriberInterface {
     parent::postSave($storage, $update);
 
     // Copy values for shared fields to existing user.
-    if (\Drupal::config('simplenews.settings')->get('subscriber.sync_fields') && $user = $this->getUser()) {
+    if (!static::$syncing === TRUE && \Drupal::config('simplenews.settings')->get('subscriber.sync_fields') && $user = $this->getUser()) {
       static::$syncing = TRUE;
       foreach ($this->getUserSharedFields($user) as $field_name) {
         $user->set($field_name, $this->get($field_name)->getValue());
